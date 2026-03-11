e92336970486b31eb527105622e5e7feca77b695
Mrunali Bind <mrunalibind123@gmail.com>
2026-03-11 22:07:56 +0530

    change url..

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ["https://interview-ai-frontend-i78v.onrender.com", "http://localhost:5173/"],
+    origin: ["https://interview-ai-frontend-i78v.onrender.com", "http://localhost:5173"],
     credentials: true
 }))
 
